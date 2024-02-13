62aef540e89e0166b6f9a4e70135490e27b79068
Param Birje <parambirje@gmail.com>
2024-02-13 13:54:41 +0530

    First commit with Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">Parable Rhythm</h1>
+    </main>
+  );
+}
+
+export default App;
